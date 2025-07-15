e09b40734dcd0270d86d68615c311ec69e0e953d
KalmiaLatifolia <lauraberman11@gmail.com>
2025-07-15 15:03:07 -0500

    add steps

--- test.R
+++ test.R
@@ -1,2 +1,11 @@
 
-library(janitor)
+# learning how to use Git
+#https://rfortherestofus.com/2021/02/how-to-use-git-github-with-r
+
+library(usethis)
+use_git()
+create_github_token()
+library(gitcreds)
+gitcreds_set()
+
+use_github()
